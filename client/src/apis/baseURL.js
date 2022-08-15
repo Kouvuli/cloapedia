86e9c338d440a21ed9dfe8a6c114171dca042646
Kouvuli <Kouvuli@users.noreply.github.com>
2022-08-15 12:03:48 +0700

    update client server baseURL commit

--- client/src/apis/baseURL.js
+++ client/src/apis/baseURL.js
@@ -2,4 +2,4 @@ export const GUARDIAN_API = "https://content.guardianapis.com"
 
 export const GUARDIAN_NEXT_GEN_API = "https://api.nextgen.guardianapps.co.uk"
 
-export const CLOAPEDIA_API = "http://localhost:9090/api/v1"
+export const CLOAPEDIA_API = "https://cloapedia-server.herokuapp.com/api/v1"
